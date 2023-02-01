af35a7cd5303a36c131a3752ea825ea8a762377f
Kevin Souza <kevin08@outlook.com.br>
2023-02-01 00:35:27 -0300

    add SignOut feature

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -90,8 +90,15 @@ function AuthProvider({ children }) {
         }
     }
 
+    async function signOut() {
+        await AsyncStorage.clear()
+        .then(() => {
+            setUser(null);
+        })
+    }
+
     return(
-        <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loadingAuth, loading}}>
+        <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, signOut, loadingAuth, loading}}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+import { AuthContext } from '../../contexts/auth';
 
 function Home() {
+    const { signOut, user } = useContext(AuthContext);
+
     return(
         <View>
             <Text>TELA HOME</Text>
+            <Text>Nome: {user.name}</Text>
+            <Button 
+                title="Sair"
+                onPress={() => signOut()}
+            />
         </View>
     )
 }
