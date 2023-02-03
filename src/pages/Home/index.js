a12ee8868323e783760e317631dd04ddf9f1c0e5
Kevin Souza <kevin08@outlook.com.br>
2023-02-02 21:20:35 -0300

    add custom header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,15 @@
 import React, { useContext } from 'react';
 import { View, Text, Button } from 'react-native';
 import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
+import { Background } from './styles';
 
 function Home() {
-    const { signOut, user } = useContext(AuthContext);
 
     return(
-        <View>
-            <Text>TELA HOME</Text>
-            <Text>Nome: {user.name}</Text>
-            <Button 
-                title="Sair"
-                onPress={() => signOut()}
-            />
-        </View>
+        <Background>
+            <Header title="Minhas movimentações"/>
+        </Background>
     )
 }
 
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -6,7 +6,22 @@ const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
     return(
-        <AppDrawer.Navigator>
+        <AppDrawer.Navigator
+            screenOptions={{
+                headerShown: false,
+
+                drawerStyle:{
+                    backgroundColor: '#FFF',
+                    paddingTop: 20,
+                },
+
+                drawerActiveBackgroundColor: '#3b3dbf',
+                drawerActiveTintColor: '#FFF',
+
+                drawerInactiveBackgroundCOlor: '#F0F2FF',
+                drawerInactiveTintColor: '#121212'
+            }}
+        >
             <AppDrawer.Screen 
                 name="Home" 
                 component={Home} 
